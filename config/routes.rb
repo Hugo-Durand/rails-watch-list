ce67762760cb6b308a4a6ca07f8dbd8f8bae8102
Hugo Durand <hugoonedu30@gmail.com>
2023-08-17 15:21:27 +0200

    Exo 2 Progress fin de journée

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/resources'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'movies#index'
 end
